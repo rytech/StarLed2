6b4b53015566c56410c8831f7bdfafb0c0811e1f
ElJay <rytech@talk21.com>
2023-08-19 13:14:26 +0100

    Add project files.

---
+++ StarLed2/StarLed2.ino
@@ -0,0 +1,16 @@
+/*
+ Name:		StarLed2.ino
+ Created:	8/18/2023 9:28:21 PM
+ Author:	rytec
+*/
+
+// the setup function runs once when you press reset or power the board
+#include <FastLED.h>
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
